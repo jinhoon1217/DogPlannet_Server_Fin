70c2a51a060ca4b2823723ff87bfdd014b4dfb12
jinhoon1217 <96006216+jinhoon1217@users.noreply.github.com>
2023-02-07 21:57:28 +0900

    Update board.js

--- routes/board.js
+++ routes/board.js
@@ -1,55 +1,26 @@
-const express = require('express');
-const multer = require('multer');
-const path = require('path');
-const fs = require('fs');
-const { Post} = require('../models');
-const { isLoggedIn } = require('../../../config/jwtMiddleware');
-
+const express = require("express");
+const { route } = require("..");
 const router = express.Router();
+const upload = require("../../module/multer");
+const postCtr = require('../../controller/posts');
 
-// uploads 폴더 없으면 생성
-try {
-  fs.readdirSync('uploads');
-} catch (error) {
-  console.error('uploads 폴더가 없어 uploads 폴더를 생성합니다.');
-  fs.mkdirSync('uploads');
-}
-
-const upload = multer({
-  storage: multer.diskStorage({ // 어디에 저장할지
-    destination(req, file, cb) {
-      cb(null, 'uploads/');
-    },
-    filename(req, file, cb) { // 어떤 이름으로 저장할지
-      const ext = path.extname(file.originalname);
-      cb(null, path.basename(file.originalname, ext) + Date.now() + ext);
-    },
-  }),
-  limits: { fileSize: 5 * 1024 * 1024 }, // 파일 크기 제한
+// 1. create
+router.get('/upload', checkUser, (req, res) => {
+    res.render('upload');
 });
 
-// 이미지 업로드
-router.post('/img', isLoggedIn, upload.single('img'), (req, res) => {
-  console.log(req.file);
-  res.json({ url: `/img/${req.file.filename}` });
-});
+// 2. read 
+router.get("/", postCtr.list);  // 게시물 전체 조회.
+router.get("/:id", postCtr.detail);  // 게시물 하나 조회
 
-// 게시글 업로드
-const upload2 = multer();
-router.post('/', isLoggedIn, upload2.none(), async (req, res, next) => {
-  try {
-    console.log(req.user);
-    const post = await Post.create({ // 게시글 저장
-      content: req.body.content,
-      img: req.body.url, // 업로드한 이미지 주소
-      UserId: req.user.id,
-    });
-    }
-    res.redirect('/');
-  } catch (error) {
-    console.error(error);
-    next(error);
-  }
-});
+// 3. update
+// web에서 form 형태로 데이터를 전송하면 get / post 두가지 밖에 쓸 수 없기 때문에
+// update에 PUT이 아니라 POST 메서드를 사용하여 요청 보냄.
+router.post('/update:id', postCtr.updateLayout);
+router.post('/', upload.single("image"), postCtr.upload);
+router.post('/update:id', postCtr.update);
+
+// 4. delete
+router.post('/delete/:id', postCtr.delete);
 
 module.exports = router;
