8542c961fa3ffded8882a03363e68c579d1bb934
jinhoon1217 <96006216+jinhoon1217@users.noreply.github.com>
2023-02-07 15:51:55 +0900

    Update board.js

--- routes/board.js
+++ routes/board.js
@@ -2,8 +2,8 @@ const express = require('express');
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
-const { Post, Hashtag } = require('../models');
-const { isLoggedIn } = require('./middlewares');
+const { Post} = require('../models');
+const { isLoggedIn } = require('../../../config/jwtMiddleware');
 
 const router = express.Router();
 
